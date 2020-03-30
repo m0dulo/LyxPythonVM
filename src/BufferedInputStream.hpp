33e12ebe009cfa0b66adf07dc5b8c4555df7ef58
m0dulo <cuckooegret@gmail.com>
2020-03-30 23:41:52 +0800

    read pyc

---
+++ src/BufferedInputStream.hpp
@@ -0,0 +1,54 @@
+#ifndef BUFFERED_INPUT_STREAM_HPP_
+#define BUFFERED_INPUT_STREAM_HPP_
+
+#include <stdio.h>
+
+#define BUFFER_LEN 256
+
+class BufferedInputStraem {
+private:
+    FILE *fp;
+    char szBuffer[BUFFER_LEN];
+    unsigned short index;
+public:
+    BufferedInputStraem(char const* filename) {
+        fp = fopen(filename, "rb");
+        fread(szBuffer, BUFFER_LEN * sizeof(char), 1, fp);
+        index = 0;
+    }
+
+    ~BufferedInputStraem() {
+        close();
+    }
+
+    char read() {
+        if (index < BUFFER_LEN)
+            return szBuffer[index++];
+        else {
+            index = 0;
+            fread(szBuffer, BUFFER_LEN * sizeof(char), 1, fp);
+            return szBuffer[index++];
+        }
+    }
+
+    int read_init() {
+        int b1 = read() & 0xff;
+        int b2 = read() & 0xff;
+        int b3 = read() & 0xff;
+        int b4 = read() & 0xff;
+
+        return b4 << 24 | b3 << 16 | b2 << 8 | b1;
+    }
+
+    void unread() {
+        index--;
+    }
+
+    void close() {
+        if (fp != NULL) {
+            fclose(fp);
+            fp = NULL;
+        }
+    }
+};
+#endif // BUFFERED_INPUT_STREAM_HPP_
---
+++ src/main.cc
@@ -0,0 +1,13 @@
+#include "BufferedInputStream.hpp"
+
+int main(int argc, char* argv[]) {
+    if (argc <= 1) {
+        printf("LyxPythonVM need  a parameter: filename\n");
+        return 0;
+    }
+
+    BufferedInputStraem stream(argv[1]);
+    printf("magic number is 0x%x\n", stream.read_init());
+
+    return 0;
+}
