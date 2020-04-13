289d81286da381cb3d8fe7ceb66d1edb1d70fece
m0dulo <cuckooegret@gmail.com>
2020-04-13 14:10:18 +0800

    add Map template class

--- src/util/map.cpp
+++ src/util/map.cpp
@@ -17,5 +17,69 @@ MapEntry<K, V>::MapEntry(const MapEntry<K, V>& entry) {
 
 template <typename K, typename V>
 void Map<K, V>::put(K k, V v) {
-    for ()
+    for (int i = 0; i < _size; ++i) {
+        if (_entries[i]._k->equal(k) == Universe::LyxTrue) {
+            _entries[i]._v = v;
+            return;
+        }
+    }
+    expand();
+    _entries[_size++] = MapEntry<K ,V>(k, v);
 }
+
+template <typename K, typename V>
+bool Map<K, V>::has_key(K k) {
+    int i = index(k);
+    return i >= 0;
+}
+
+template <typename K, typename V>
+V Map<K, V>::get(K k) {
+    int i = index(k);
+    if (i < 0)
+        return Universe::LyxFalse;
+    else 
+        return _entries[i]._v;
+}
+
+template <typename K, typename V>
+int Map<K, V>::index(K k) {
+    for (int i = 0; i < _size; ++i) {
+        if (_entries[i]._k->equal(k) == Universe::LyxTrue) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+template <typename K, typename V>
+void Map<K, V>::expand() {
+    if (_size > _length) {
+        MapEntry<K, V>* new_entries = new MapEntry<K, V>[length << 1];
+        for (int i = 0; i < _size; ++i) {
+            new_entries[i] = _entries[i];
+        }
+        _length <<= 1;
+        delete[] _entries;
+        _entries = new_entries;
+    }
+}
+
+template <typename K, typename V>
+V Map<K, V>::remove(K k) {
+    int i = index(i);
+
+    if (i < 0)
+        return 0;
+    V v = _entries[i]._v;
+    _entries[i] = _entries[--_size];
+    return v;
+}
+
+template <typename K, typename V>
+K Map<K, V>::get_key(int index) {
+    return _entries[index].k;
+}
+
+template class Map<LyxObject*, LyxObject*>;
