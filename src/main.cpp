5df2cc6377f676fa055e77070dbfca7e86937332
m0dulo <cuckooegert@gmail.com>
2020-09-27 12:09:30 +0800

    add compile_commands.json to gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]) {
         printf("LyxPythonVM need  a parameter: filename\n");
         return 0;
     }
-    
+
     BufferedInputStraem stream(argv[1]);
     BinaryFileParser parser(&stream);
     CodeObject* main_code = parser.parse();
